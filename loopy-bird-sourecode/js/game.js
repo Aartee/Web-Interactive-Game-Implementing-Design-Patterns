5483b6878e9ed0fcbe4b035b02f5fbac31c39643
prathmesh-parmar <31287314+prathmesh-parmar@users.noreply.github.com>
2017-10-27 19:17:48 -0700

    Contains all the resources for the images and the on load and loaded function

---
+++ loopy-bird-sourecode/js/game.js
@@ -0,0 +1,50 @@
+var game = {
+    data: {
+        score : 0,
+        steps: 0,
+        start: false,
+        newHiScore: false,
+    },
+
+    resources: [
+            // images
+        {name: "bg", type:"image", src: "data/img/bg.png"},
+        {name: "clumsy", type:"image", src: "data/img/clumsy.png"},
+        {name: "pipe", type:"image", src: "data/img/pipe.png"},
+        {name: "logo", type:"image", src: "data/img/logo.png"},
+        {name: "ground", type:"image", src: "data/img/ground.png"},
+        {name: "gameover", type:"image", src: "data/img/gameover.png"},
+        {name: "gameoverbg", type:"image", src: "data/img/gameoverbg.png"},
+        {name: "hit", type:"image", src: "data/img/hit.png"},
+        {name: "getready", type:"image", src: "data/img/getready.png"},
+        {name: "new", type:"image", src: "data/img/new.png"}
+    ],
+
+    "onload": function() {
+        if (!me.video.init(900, 600, {
+            wrapper: "screen",
+            scale : "auto",
+            scaleMethod: "fit"
+        })) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+        me.loader.preload(game.resources, this.loaded.bind(this));
+    },
+
+    "loaded": function() {
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+        me.state.set(me.state.GAME_OVER, new game.GameOverScreen());
+
+        me.input.bindKey(me.input.KEY.SPACE, "fly", true);
+        me.input.bindPointer(me.input.KEY.SPACE);
+
+        me.pool.register("clumsy", game.BirdEntity);
+        me.pool.register("pipe", game.PipeEntity, true);
+        me.pool.register("hit", game.HitEntity, true);
+        me.pool.register("ground", game.Ground, true);
+
+        me.state.change(me.state.MENU);
+    }
+};
