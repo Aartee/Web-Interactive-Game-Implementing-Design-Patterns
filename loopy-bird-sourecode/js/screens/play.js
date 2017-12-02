8a978f899c10b27ec8ce48dfd5c14b17b8dd0568
Aartee Kasliwal <aarteekasliwal@gmail.com>
2017-12-02 11:19:01 -0800

    Update play.js

--- loopy-bird-sourecode/js/screens/play.js
+++ loopy-bird-sourecode/js/screens/play.js
@@ -1,7 +1,7 @@
-//Modular Design Pttern
-//Here Private members are kept in the closure.
-//Public members are exposed in the return object.
-//Like returning Entity object.
+/* COMPOSITE DESIGN PATTERN
+ * Component: Game
+ * Leaf: PlayScreen
+ */
 game.PlayScreen = me.ScreenObject.extend({
     init: function() {
         me.audio.play("theme", true);
